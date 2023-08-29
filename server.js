bc3208e581a445ea8f95c035b0a925f09cc775ad
BogdanDarlau <darlau.bogdan@yahoo.com>
2023-08-29 16:38:05 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const PORT = process.env.PORT || 3001; // Scegli una porta per il tuo server
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // o specifica il dominio del tuo frontend
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.get("/get-gist-data", async (req, res) => {
+  try {
+    const gistUrl =
+      "https://gist.github.com/BogdanDarlau/fa20eee5fd7085085611bacf285126b3";
+    const response = await axios.get(gistUrl);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Errore nel recupero dei dati dal Gist" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
