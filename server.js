0e21bec26d274d4bacbb008529b588b1329502b2
BogdanDarlau <125072511+BogdanDarlau@users.noreply.github.com>
2024-01-08 18:44:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 3300;
+const port = process.env.PORT ;
 const bodyParser = require("body-parser");
 const pgp = require("pg-promise");
 
